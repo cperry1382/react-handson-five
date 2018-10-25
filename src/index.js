4669d0c3b383c8154344cd27f8b28a693a491fba
Christopher Perry <c.perry@icloud.com>
2018-10-25 11:26:07 -0700

    final commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './screens/Home';
+import About from './screens/About';
+import Things from './screens/Things';
+import FavoriteThings from './screens/FavoriteThings';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const App = () => (
+
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+        <li>
+          <Link to="/FavoriteThings">FavoriteThings</Link>
+        </li>
+      </ul>
+
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/About" component={About} />
+      <Route path="/FavoriteThings" component={FavoriteThings} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
